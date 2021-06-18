651c97ab99f677bbe5782ae984db62266e2ccdd2
Sregey Slezhov <pishuhott@gmail.com>
2021-06-18 18:24:26 +0300

    fix: fixed screen adaptation, fixed slider

--- js/main.js
+++ js/main.js
@@ -23,13 +23,14 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector('.burger').classList.remove('burger-active');
         });
 
-    };
+    }
+    ;
 
     const observer = new IntersectionObserver((entries) => {
         entries.forEach((entry) => {
             if (entry.isIntersecting) {
                 document.querySelectorAll('.navigation__link').forEach((link) => {
-                    link.classList.toggle('navigation__link-active', 
+                    link.classList.toggle('navigation__link-active',
                         link.getAttribute('href').substr(1) === entry.target.id);
                 });
             }
@@ -41,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('.navigation-section').forEach((section) => {
         observer.observe(section)
     });
-  
+
     //====BURGER-MENU====
     (function () {
         const burgerMenu = document.querySelector('.burger');
@@ -68,7 +69,8 @@ document.addEventListener("DOMContentLoaded", function () {
                     item.classList.remove('active-tab')
                 });
                 currentBtn.classList.add('active-tab');
-            };
+            }
+            ;
             let wrapper = document.querySelector('.portfolio-gallery');
 
             let selectors = wrapper.children;
@@ -77,7 +79,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
             for (let i = 0; i < selectors.length; i++) {
                 wrapper.appendChild(selectors[i]);
-            };
+            }
+            ;
         });
     });
 
@@ -88,62 +91,88 @@ document.addEventListener("DOMContentLoaded", function () {
     };
 
     //=====Slider======
-    (function () {
-        let doc = document;
-        let index = 1;
-        let Slider = function () {
-            this.box = doc.querySelector('.swiper-container');
-            this.slidesBox = doc.querySelector('.swiper-elements');
-            this.slides = doc.querySelectorAll('.slide');
-            this.btns = doc.querySelectorAll('.btn');
-            this.size = this.box.clientWidth;
+    const images = document.querySelectorAll('.slide');
+    const swipeBts = document.querySelectorAll('.btn');
+    let imagesArr = [];
 
-            this.position();
-            this.carousel();
+    for (let i = 0; i < images.length; i++) {
+        imagesArr[i] = images[i].src;
+        images[i].remove()
+    }
 
-        };
+    let step = 0;
+    let offset = 0;
+    let width;
+    let img;
+    function draw(pos) {
+        img = document.createElement('img');
+        width = document.querySelector('.swiper-container').offsetWidth
+        img.src = imagesArr[step];
+        img.classList.add('slide');
+        img.style.left = offset * width + 'px';
+        img.style.width = width +'px';
+        img.style.height = 'auto';
 
-        Slider.prototype.position = function () {
-            let size = this.size;
-            this.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-        };
+        if (pos == 'left') {
+            document.querySelector('.swiper-elements').appendChild(img)
+        } else {
+            document.querySelector('.swiper-elements').prepend(img)
+        }
 
-        Slider.prototype.carousel = function () {
-            let i, max = this.btns.length,
-                that = this;
+        let images2 = document.querySelectorAll('.slide');
+        let offset2 = 0;
+        images2.forEach(img => {
+            img.style.left = offset2 * width - width + 'px';
+            offset2++;
+        })
 
-            for (i = 0; i < max; i += 1) {
-                that.btns[i].addEventListener('click', Slider[that.btns[i].id].bind(null, that));
+        if (step + 1 == images.length) {
+            step = 0;
+        } else {
+            step++;
+        }
+        offset = 1;
+    }
+
+    function imgResize() {
+        let images2 = document.querySelectorAll('.slide');
+        width = document.querySelector('.swiper-container').offsetWidth
+        images2.forEach(img => {
+            img.style.width = width +'px';
+            img.style.height = 'auto';
+        })
+    }
+
+    window.addEventListener('resize', imgResize)
+
+    function slider(imgId, pos) {
+        let images2 = document.querySelectorAll('.slide');
+        let offset2 = 0;
+        images2.forEach(img => {
+
+            if (pos === 'left') {
+                img.style.left = offset2 * width - width + 'px';
+                offset2++;
+            } else {
+                img.style.left = offset2 * width + width + 'px';
+                offset2--;
             }
-        }
+        })
+        images2[imgId].remove();
+        draw(pos)
+    }
 
-        Slider.prev = function (box) {
-            box.slidesBox.style.transition = "transform .3s ease-in-out";
-            let size = box.size;
-            index <= 0 ? false : index--;
-            box.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-            box.jump();
-        };
+    draw('left'); draw('left'); draw('left');
 
-        Slider.next = function (box) {
-            box.slidesBox.style.transition = "transform .3s ease-in-out";
-            let max = box.slides.length;
-            let size = box.size;
-            index >= max - 1 ? false : index++;
-            box.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-            box.jump();
-        };
-
-        Slider.prototype.jump = function () {
-            let that = this;
-            let size = this.size;
-            this.slidesBox.addEventListener('transitionend', function () {
-                that.slides[index].id === "firstClone" ? index = 1 : index;
-                that.slides[index].id === "lastClone" ? index = that.slides.length - 2 : index;
-                that.slidesBox.style.transition = "none";
-                that.slidesBox.style.transform = 'translateX(' + (-index * size) + 'px)';
-            });
-        }
-        new Slider();
-    })();
+    swipeBts.forEach(btn => {
+        btn.addEventListener('click', () => {
+            if (btn.id === 'next') {
+                console.log('next');
+                slider(0, 'left')
+            } else {
+                console.log('prev');
+                slider(2, 'right')
+            }
+        })
+    })
 });
